05274401d7198b8f3c9b0d7d10ee24afd267e041
Ryan Yep <ryan.yep@gmail.com>
2015-09-01 14:51:44 -0500

    Working user prompt w/ correct logic

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -9,21 +9,24 @@ class GuessingGame
   end
 
   def comp_num(guess_num)
-    @comp_num = SecureRandom.random_number(100)                                                    # => 65
+    @comp_num = SecureRandom.random_number(100)                                                    # => 44
     if guess_num == @comp_num                                                                      # => false
       puts "Congrats, you've won! GuessNum: #{guess_num}, CompNum: #{@comp_num}"
-    elsif guess_num < @comp_num                                                                    # => true
-      puts "You've guessed low! Please try again. GuessNum: #{guess_num}, CompNum: #{@comp_num}"   # => nil
-    elsif guess_num > @comp_num
-      puts "You've guessed high! Please try again. GuessNum: #{guess_num}, CompNum: #{@comp_num}"
+    elsif guess_num < @comp_num                                                                    # => false
+      puts "You've guessed low! Please try again. GuessNum: #{guess_num}, CompNum: #{@comp_num}"
+    elsif guess_num > @comp_num                                                                    # => true
+      puts "You've guessed high! Please try again. GuessNum: #{guess_num}, CompNum: #{@comp_num}"  # => nil
     else
       puts "What are you doing?!"
     end                                                                                            # => nil
   end
+
 end
 
-guess_num = GuessingGame.new  # => #<GuessingGame:0x007fa66184ecc0 @comp_num=0>
-puts guess_num.comp_num(50)   # => nil
+guess_num = GuessingGame.new  # => #<GuessingGame:0x007fe561868af0 @comp_num=0>
 
-# >> You've guessed low! Please try again. GuessNum: 50, CompNum: 65
-# >>
+print "Please guess a number between 1 and 100: > "
+ask_user = gets.chomp.to_i
+guess_num.comp_num(ask_user)        # => nil
+
+# >> You've guessed high! Please try again. GuessNum: 50, CompNum: 44
