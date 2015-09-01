9d9aed354e309a1bc501bbbd4c1b1aabf0d1f950
Ryan Yep <ryan.yep@gmail.com>
2015-09-01 15:03:45 -0500

    includes exit from game when user wins

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -9,24 +9,21 @@ class GuessingGame
   end
 
   def comp_num(guess_num)
-    @comp_num = SecureRandom.random_number(100)                                                    # => 44
-    if guess_num == @comp_num                                                                      # => false
-      puts "Congrats, you've won! GuessNum: #{guess_num}, CompNum: #{@comp_num}"
-    elsif guess_num < @comp_num                                                                    # => false
+    @comp_num = SecureRandom.random_number(100)                                                    # => 41
+    if guess_num < @comp_num                                                                             # => false
       puts "You've guessed low! Please try again. GuessNum: #{guess_num}, CompNum: #{@comp_num}"
-    elsif guess_num > @comp_num                                                                    # => true
-      puts "You've guessed high! Please try again. GuessNum: #{guess_num}, CompNum: #{@comp_num}"  # => nil
-    else
-      puts "What are you doing?!"
-    end                                                                                            # => nil
+    elsif guess_num > @comp_num                                                                          # => false
+      puts "You've guessed high! Please try again. GuessNum: #{guess_num}, CompNum: #{@comp_num}"
+    elsif guess_num == @comp_num                                                                          # => true
+      puts "Congrats, you've won! GuessNum: #{guess_num}, CompNum: #{@comp_num}"                   # => nil
+      exit                                                                                     # ~> NameError: uninitialized constant GuessingGame::Exit
+    end
   end
-
 end
 
-guess_num = GuessingGame.new  # => #<GuessingGame:0x007fe561868af0 @comp_num=0>
+guess_num = GuessingGame.new  # => #<GuessingGame:0x007fe33a102f00 @comp_num=0>
 
-print "Please guess a number between 1 and 100: > "
-ask_user = gets.chomp.to_i
-guess_num.comp_num(ask_user)        # => nil
+print "Please guess a number between 1 and 100: > "  # => nil
+ask_user = gets.chomp.to_i                           # ~> NoMethodError: undefined method `chomp' for nil:NilClass
+guess_num.comp_num(ask_user)
 
-# >> You've guessed high! Please try again. GuessNum: 50, CompNum: 44
